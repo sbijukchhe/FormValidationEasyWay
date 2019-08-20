dd2be37bd53d5a6fcac80992f466cacb35c99138
sbijukchhe <skbijukchhe@gmail.com>
2019-08-20 10:28:00 -0400

    Handling Form Values using Thymeleaf

---
+++ src/main/java/com/example/demo/FormValidationEasyWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidationEasyWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormValidationEasyWayApplication.class, args);
+    }
+
+}
